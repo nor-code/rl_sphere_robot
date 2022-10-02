2ac581f048c06b2b5cb97a04e202fd64bf1c209d
Нор Никита <nikita.nor@gazprombank.ru>
2022-10-03 00:18:51 +0300

    remove line

--- run.sh
+++ run.sh
@@ -2,9 +2,6 @@ mkdir models
 
 tensorboard --logdir=runs &
 
-# плохой способ loss падает только под конец всего обучения
-nohup python3 main.py --simu_number=1 --type_task=3 --algo=ddqn --trajectory=circle --buffer_size=10000000 --batch_size=16384 --refresh_target=1600 --total_steps=500000 --decay_steps=500000 > out1.log &
-
 nohup python3 main.py --simu_number=1 --type_task=4 --algo=ddqn --trajectory=circle --buffer_size=6000000 --batch_size=4096 --refresh_target=400 --total_steps=20000 --decay_steps=5000 > out1.log &
 
 nohup python3 main.py --simu_number=2 --type_task=4 --algo=ddpg --trajectory=circle --buffer_size=6000000 --batch_size=4096 --total_steps=20000 --decay_steps=5000 > out2.log &
