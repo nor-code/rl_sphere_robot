b81438e542068be704a03a01da820a4ececf5ab1
Нор Никита <nikita.nor@gazprombank.ru>
2022-10-08 00:52:35 +0300

    change freq_train -> every 32 new lines in replay buffer

--- agent/dqn.py
+++ agent/dqn.py
@@ -127,7 +127,7 @@ class DeepQLearningAgent(nn.Module):
     def get_learn_freq(self):
         if self.replay_buffer.buffer_len() >= self.replay_buffer.get_maxsize():
             return 16
-        return 16
+        return 32
 
     def play_episode(self, initial_state, enviroment, episode_timeout, n_steps, global_iteration, episode):
         s = initial_state
