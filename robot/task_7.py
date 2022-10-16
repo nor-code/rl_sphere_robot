ff29c62ebad8fd0a3ff04a85cae9d35e675f4033
Нор Никита <nikita.nor@gazprombank.ru>
2022-10-16 16:25:33 +0300

    exchange task_7

--- robot/task_7.py
+++ robot/task_7.py
@@ -26,6 +26,7 @@ class TrakingTrajectoryTask7(base.Task):
         """ целевая траектория и начальная точка на ней """
         self.point_x_y = trajectory_x_y
         self.points = np.copy(self.point_x_y, order='K')
+
         self.begin_index = begin_index
         self.no_return_index = self.get_index(begin_index - 2)
 
@@ -33,11 +34,13 @@ class TrakingTrajectoryTask7(base.Task):
         self.index2 = self.get_index(begin_index - 1)
         self.index3 = self.get_index(begin_index)
         self.index4 = self.get_index(begin_index + 1)
+        self.index5 = self.get_index(begin_index + 2)
 
         self.prev_index1 = self.get_index(begin_index - 2)
         self.prev_index2 = self.get_index(begin_index - 1)
         self.prev_index3 = self.get_index(begin_index)
         self.prev_index4 = self.get_index(begin_index + 1)
+        self.prev_index4 = self.get_index(begin_index + 2)
 
         self.dist = 0.0
         self.robot_position = [self.points[begin_index][0], self.points[begin_index][1]]
@@ -67,7 +70,7 @@ class TrakingTrajectoryTask7(base.Task):
         index = self.begin_index
         print("begin index = ", index)
 
-        self.get_nearest_4_points_index()
+        self.get_nearest_5_points_index()
         self.no_return_index = self.index1
         self.dist = np.linalg.norm([x - self.points[self.no_return_index][0],
                                     y - self.points[self.no_return_index][1]])
@@ -81,26 +84,38 @@ class TrakingTrajectoryTask7(base.Task):
 
         self.dist = 0.0
 
-        r1 = norm([x - self.points[self.index1][0], y - self.points[self.index1][1]])
-        r2 = norm([x - self.points[self.index2][0], y - self.points[self.index2][1]])
-        r3 = norm([x - self.points[self.index3][0], y - self.points[self.index3][1]])
-        r4 = norm([x - self.points[self.index4][0], y - self.points[self.index4][1]])
+        v_r1 = vector(self.points[self.index1], self.robot_position)
+        v_r2 = vector(self.points[self.index2], self.robot_position)
+        v_r3 = vector(self.points[self.index3], self.robot_position)
+        v_r4 = vector(self.points[self.index4], self.robot_position)
+        v_r5 = vector(self.points[self.index5], self.robot_position)
 
-        prev_r1 = norm([x - self.points[self.prev_index1][0], y - self.points[self.prev_index1][1]])
-        prev_r2 = norm([x - self.points[self.prev_index2][0], y - self.points[self.prev_index2][1]])
-        prev_r3 = norm([x - self.points[self.prev_index3][0], y - self.points[self.prev_index3][1]])
-        prev_r4 = norm([x - self.points[self.prev_index4][0], y - self.points[self.prev_index4][1]])
+        v_prev_r1 = vector(self.points[self.prev_index1], self.robot_position)
+        v_prev_r2 = vector(self.points[self.prev_index2], self.robot_position)
+        v_prev_r3 = vector(self.points[self.prev_index3], self.robot_position)
+        v_prev_r4 = vector(self.points[self.prev_index4], self.robot_position)
+        v_prev_r5 = vector(self.points[self.prev_index5], self.robot_position)
 
-        self.state = [r1, r2, r3, r4, 0.0, 0.0, prev_r1, prev_r2, prev_r3, prev_r4, 0.0, 0.0]
+        v_i1_i2 = vector(self.points[self.index1], self.points[self.index2])
+        v_i2_i3 = vector(self.points[self.index2], self.points[self.index3])
+        v_i3_i4 = vector(self.points[self.index3], self.points[self.index4])
+        v_i4_i5 = vector(self.points[self.index4], self.points[self.index5])
+
+        self.state = [v_r1[0], v_r1[1], v_r2[0], v_r2[1], v_r3[0],
+                      v_r3[1], v_r4[0], v_r4[1], v_r5[0], v_r5[1],
+                      v_prev_r1[0], v_prev_r1[1], v_prev_r2[0], v_prev_r2[1], v_prev_r3[0],
+                      v_prev_r3[1], v_prev_r4[0], v_prev_r4[1], v_prev_r5[0], v_prev_r5[1],
+                      v_i1_i2[0], v_i1_i2[1], v_i2_i3[0], v_i2_i3[1],
+                      v_i3_i4[0], v_i3_i4[1], v_i4_i5[0], v_i4_i5[1]]
         super().initialize_episode(physics)
 
-    def get_nearest_4_points_index(self):
+    def get_nearest_5_points_index(self):
         x, y = self.robot_position
         dist = np.array([np.sqrt((x - point[0]) ** 2 + (y - point[1]) ** 2) for point in self.points])
 
-        arr = dist.argsort()[:4]
+        arr = dist.argsort()[:5]
         brr = arr - arr.max()
-        indexes = np.where(brr <= -5)
+        indexes = np.where(brr <= -6)
 
         if indexes[0].shape[0] != 0:
             index = np.where(arr == np.max(arr[indexes]))[0]
@@ -116,11 +131,13 @@ class TrakingTrajectoryTask7(base.Task):
         self.prev_index2 = self.index2
         self.prev_index3 = self.index3
         self.prev_index4 = self.index4
+        self.prev_index5 = self.index5
 
         self.index1 = arr[0]
         self.index2 = arr[1]
         self.index3 = arr[2]
         self.index4 = arr[3]
+        self.index5 = arr[4]
 
         if (self.no_return_index == len(
                 self.points) - 1 and self.prev_index1 == 0) or self.no_return_index < self.prev_index1:
@@ -137,32 +154,30 @@ class TrakingTrajectoryTask7(base.Task):
 
         self.robot_position = [x, y]
 
-        self.get_nearest_4_points_index()
+        self.get_nearest_5_points_index()
 
         v_r1 = vector(self.points[self.index1], self.robot_position)
         v_r2 = vector(self.points[self.index2], self.robot_position)
         v_r3 = vector(self.points[self.index3], self.robot_position)
         v_r4 = vector(self.points[self.index4], self.robot_position)
+        v_r5 = vector(self.points[self.index5], self.robot_position)
 
         v_prev_r1 = vector(self.points[self.prev_index1], self.robot_position)
         v_prev_r2 = vector(self.points[self.prev_index2], self.robot_position)
         v_prev_r3 = vector(self.points[self.prev_index3], self.robot_position)
         v_prev_r4 = vector(self.points[self.prev_index4], self.robot_position)
+        v_prev_r5 = vector(self.points[self.prev_index5], self.robot_position)
 
-        r1, r2, r3, r4 = norm(v_r1), norm(v_r2), norm(v_r3), norm(v_r4)
-        prev_r1, prev_r2, prev_r3, prev_r4 = norm(v_prev_r1), norm(v_prev_r2), norm(v_prev_r3), norm(v_prev_r4)
-
-        v_i1_i2 = vector(self.points[self.index2], self.points[self.index1])
-        v_i2_i3 = vector(self.points[self.index3], self.points[self.index2])
-        sin_alpha1 = np.sin(round(get_angle_between_2_vector(v_i1_i2, v_r1, norm(v_i1_i2), r1), 4))
-        sin_alpha2 = np.sin(round(get_angle_between_2_vector(v_i2_i3, v_r2, norm(v_i2_i3), r2), 4))
-
-        v_prev_i1_i2 = vector(self.points[self.prev_index2], self.points[self.prev_index1])
-        v_prev_i2_i3 = vector(self.points[self.prev_index3], self.points[self.prev_index2])
-        sin_prev_alpha1 = np.sin(round(get_angle_between_2_vector(v_prev_i1_i2, v_prev_r1, norm(v_prev_i1_i2), prev_r1), 4))
-        sin_prev_alpha2 = np.sin(round(get_angle_between_2_vector(v_prev_i2_i3, v_prev_r2, norm(v_prev_i2_i3), prev_r2), 4))
-                     #0   1   2   3   4           5           6        7        8        9        10               11
-        self.state = [r1, r2, r3, r4, sin_alpha1, sin_alpha2, prev_r1, prev_r2, prev_r3, prev_r4, sin_prev_alpha1, sin_prev_alpha2]
+        v_i1_i2 = vector(self.points[self.index1], self.points[self.index2])
+        v_i2_i3 = vector(self.points[self.index2], self.points[self.index3])
+        v_i3_i4 = vector(self.points[self.index3], self.points[self.index4])
+        v_i4_i5 = vector(self.points[self.index4], self.points[self.index5])
+                    # 0           1         2        3        4        5        6        7        8        9
+        self.state = [v_r1[0],    v_r1[1],  v_r2[0], v_r2[1], v_r3[0], v_r3[1], v_r4[0], v_r4[1], v_r5[0], v_r5[1],
+                    # 10            11            12            13            14            15            16            17            18            19
+                      v_prev_r1[0], v_prev_r1[1], v_prev_r2[0], v_prev_r2[1], v_prev_r3[0], v_prev_r3[1], v_prev_r4[0], v_prev_r4[1], v_prev_r5[0], v_prev_r5[1],
+                    # 20          21          22          23          24          25          26          27
+                      v_i1_i2[0], v_i1_i2[1], v_i2_i3[0], v_i2_i3[1], v_i3_i4[0], v_i3_i4[1], v_i4_i5[0], v_i4_i5[1]]
         return self.state  # np.concatenate((xy, acc_gyro), axis=0)
 
     def get_termination(self, physics):
@@ -182,7 +197,6 @@ class TrakingTrajectoryTask7(base.Task):
 
     def get_reward(self, physics):
         state = self.state
-        x, y = self.robot_position
 
         if self.is_invalid_state_soft():
             self.count_invalid_states += 1
@@ -196,22 +210,34 @@ class TrakingTrajectoryTask7(base.Task):
             print("вернулись на траекторию")
             self.count_invalid_states = 0
 
-        r1, r2, r3, r4 = state[0], state[1], state[2], state[3]
-        prev_r1, prev_r2 = state[6], state[7]
+        v_r1 = [state[0], state[1]]
+        v_r2 = [state[2], state[3]]
+        v_r3 = [state[4], state[5]]
+        v_r4 = [state[6], state[7]]
+        v_r5 = [state[8], state[9]]
 
-        distance_reward1 = self.get_reward_for_distance(0.10, r1)
-        distance_reward2 = self.get_reward_for_distance(0.15, r2)
-        distance_reward3 = self.get_reward_for_distance(0.55, r3)
-        distance_reward4 = self.get_reward_for_distance(0.85, r4)
+        r1, r2, r3, r4, r5 = norm(v_r1), norm(v_r2), norm(v_r3), norm(v_r4), norm(v_r5)
+        i1_i2, i2_i3, i3_i4, i4_i5 = [state[20], state[21]], [state[22], state[23]], [state[24], state[25]], [state[26], state[27]]
 
-        reward = distance_reward1 + distance_reward2 + distance_reward3 + distance_reward4 - 20 * state[4] * r1 - 30 * state[5] * r2
+        sin_a1 = np.sin(round(get_angle_between_2_vector(i1_i2, v_r1, norm(i1_i2), r1), 4))
+        sin_a2 = np.sin(round(get_angle_between_2_vector(i2_i3, v_r2, norm(i2_i3), r2), 4))
+        sin_a3 = np.sin(round(get_angle_between_2_vector(i3_i4, v_r3, norm(i3_i4), r3), 4))
+        sin_a4 = np.sin(round(get_angle_between_2_vector(i4_i5, v_r4, norm(i4_i5), r4), 4))
+        sin_a5 = np.sin(round(get_angle_between_2_vector([-state[26], -state[27]], v_r5, norm(i4_i5), r5), 4))
+
+        reward1 = self.get_reward_for_distance(0.10, r1)
+        reward2 = self.get_reward_for_distance(0.15, r2)
+        reward3 = self.get_reward_for_distance(0.55, r3)
+        reward4 = self.get_reward_for_distance(0.85, r4)
+        reward5 = self.get_reward_for_distance(1.0, r5)
+
+        reward = reward1 + reward2 + reward3 + reward4 + reward5\
+                 - 10 * sin_a1 * r1 - 15 * sin_a2 * r2 - 25 * sin_a3 * r3 - 35 * sin_a4 * r4 - 50 * sin_a5 * r5
         return reward
 
     # если количество точек в окретности робота не осталось
     def is_invalid_state_soft(self):
-        state = self.state
-        x = state[0]
-        y = state[1]
+        x, y = self.robot_position
         for point in self.points:
             if np.sqrt((point[0] - x) ** 2 + (point[1] - y) ** 2) <= self.radius:
                 return False
--- test/sphere.py
+++ test/sphere.py
@@ -37,9 +37,9 @@ agent = DeepDeterministicPolicyGradient(12,
 # agent.q_network.load_state_dict(torch.load('../models/ddqn2_3.pt', map_location=torch.device('cpu')))
 # agent.q_network.eval()
 
-agent.policy.load_state_dict(torch.load('../models/ddpg_policy_2_6.pt', map_location=torch.device('cpu')))
+agent.policy.load_state_dict(torch.load('../models/ddpg_policy_1_5.pt', map_location=torch.device('cpu')))
 agent.policy.eval()
-agent.qf.load_state_dict(torch.load('../models/ddpg_Q_2_6.pt', map_location=torch.device('cpu')))
+agent.qf.load_state_dict(torch.load('../models/ddpg_Q_1_5.pt', map_location=torch.device('cpu')))
 agent.qf.eval()
 
 final_time = 0
@@ -86,7 +86,7 @@ def action_policy(time_step):
     #     return [-0.22, 0.31]
 
 
-env, x_y = make_env(episode_timeout=60, type_task=7, trajectory='circle', begin_index_=5)
+env, x_y = make_env(episode_timeout=60, type_task=7, trajectory='circle', begin_index_=10)
 app = application.Application()
 app.launch(env, policy=action_policy)
 
